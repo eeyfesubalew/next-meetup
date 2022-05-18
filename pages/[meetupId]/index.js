a24fca30b77866fe8780b227135a0414a2414b75
esubalew.seife@gmail.com <esubalew.seife@gmail.com>
2022-05-18 11:23:05 -0700

    adding custom css

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,12 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+const MeetupDetails = () => {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/en/a/a1/Free_HQ_in_Paris.jpg"
+      title="A first meetup"
+      address="Addis ababa piasa"
+      description="This is the first meet up"
+    />
+  );
+};
+export default MeetupDetails;
