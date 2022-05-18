f69af739e716cdf2cc05512d27c41ae569b14214
esubalew.seife@gmail.com <esubalew.seife@gmail.com>
2022-05-18 10:19:17 -0700

    outputing meetup pages

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,24 @@
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A first meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/en/a/a1/Free_HQ_in_Paris.jpg",
+    address: "Addis Ababa,Piassa",
+    descreption: "this is the first meetup",
+  },
+  {
+    id: "m2",
+    title: "A second meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/en/a/a1/Free_HQ_in_Paris.jpg",
+    address: "Addis Ababa,Piassa",
+    descreption: "this is the second meetup",
+  },
+];
 const HomePage = () => {
-  return <h1>This is HomePage</h1>;
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
 };
 export default HomePage;
