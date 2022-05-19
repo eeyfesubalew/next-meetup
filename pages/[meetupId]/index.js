6fa3a04ab9b0263d386b23b6a2af6b1d70450499
eeyfesubalew <esubalew.seife@gmail.com>
2022-05-18 19:07:06 -0800

    getstaticpaths and falback

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -9,4 +9,28 @@ const MeetupDetails = () => {
     />
   );
 };
+
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [{ params: { meetupId: "m1" } }, { params: { meetupId: "m1" } }],
+  };
+}
+export function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        id: meetupId,
+        image:
+          "https://upload.wikimedia.org/wikipedia/en/a/a1/Free_HQ_in_Paris.jpg",
+        title: "A first meetup",
+        address: "Addis ababa piasa",
+        description: "This is the first meet up",
+      },
+    },
+  };
+}
 export default MeetupDetails;
